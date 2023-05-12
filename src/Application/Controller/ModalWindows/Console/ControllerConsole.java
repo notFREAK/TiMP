debe482bcc6bacacb693b3746d881dcdc3638f03
notFreak <nikitapolovin2003@gmail.com>
2023-05-12 21:49:24 +0700

    Добавляю консоль

---
+++ src/Application/Controller/ModalWindows/Console/ControllerConsole.java
@@ -0,0 +1,4 @@
+package Application.Controller.ModalWindows.Console;
+
+public class ControllerConsole {
+}
---
+++ src/Application/Controller/ModalWindows/Console/FXML/FXMLObjects.java
@@ -0,0 +1,10 @@
+package Application.Controller.ModalWindows.Console.FXML;
+
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class FXMLObjects {
+    @FXML
+    TextArea console;
+}
